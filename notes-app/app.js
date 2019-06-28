d56728413297ed0ffb75a6b398aed66e8f951305
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-06-28 12:08:42 -0700

    completed second challenge

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,3 @@
-const fs = require('fs');
-
-//fs.writeFileSync('notes.txt', 'Walter Cosmopolis is World Famous');
-
-fs.appendFileSync('notes.txt', 'Walter Cosmopolis is World Famous for a reason!');
+const getNotes = require('./notes.js');
+const notes = getNotes();
+console.log(notes);
---
+++ notes-app/utils.js
@@ -0,0 +1,8 @@
+console.log(`utils.js`);
+
+const name = "Walter";
+const add = function (a,b) {
+  return a + b;
+};
+
+module.exports = add;
