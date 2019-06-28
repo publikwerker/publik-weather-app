274812ce00075afb1e34f3878444bfccf9888b2e
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-06-28 12:40:13 -0700

    finished third challenge

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,3 +1,8 @@
+const chalk = require('chalk');
 const getNotes = require('./notes.js');
 const notes = getNotes();
 console.log(notes);
+console.log(chalk.green('Success!!'));
+console.log('Yay for' + chalk.keyword('orange')(' orange')+ ' colored text!');
+console.log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
+console.log(chalk.hex('#DEADED').bold('Bold gray!'));
