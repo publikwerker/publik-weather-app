96dfec44ab97f937de33fe42c84239d3cb619f01
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-06-28 17:36:24 -0700

    completed fourth challenge

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,46 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
+
 const getNotes = require('./notes.js');
-const notes = getNotes();
-console.log(notes);
-console.log(chalk.green('Success!!'));
-console.log('Yay for' + chalk.keyword('orange')(' orange')+ ' colored text!');
-console.log(chalk.rgb(123, 45, 67).underline('Underlined reddish color'));
-console.log(chalk.hex('#DEADED').bold('Bold gray!'));
+
+//Customize yargs version
+yargs.version('12.0.2');
+
+//Create add command
+yargs.command({
+  command:'add',
+  describe: 'Add a new note',
+  handler: function(){
+    console.log('adding a new note');
+  }
+})
+
+//Create remove command
+yargs.command({
+  command: 'remove',
+  describe: 'Remove a note',
+  handler: function(){
+    console.log('Removing a note');
+  }
+})
+
+//Create list command
+yargs.command({
+  command: 'list',
+  describe: 'List notes',
+  handler: function(){
+    console.log('Listing Notes');
+  }
+})
+
+//Create read command
+yargs.command({
+  command: 'read',
+  describe: 'Read notes',
+  handler: function(){
+    console.log('Reading notes');
+  }
+})
+
+//add, remove, read, list
+console.log(yargs.argv);
