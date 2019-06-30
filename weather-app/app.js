ddb4e225ed2fdd0931b5a7cb8480d46b4f823c32
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-06-30 13:02:44 -0700

    installed request to handle HTTP requests to Darksky API, display basic info in console.

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,5 @@ const chalk = require('chalk');
 const URL ='https://api.darksky.net/forecast/479e3e61b28a471af898ef5eccc876f2/37.8267,-122.4233';
 
 request({url: URL, json: true}, (err, res)=>{
-  console.log(chalk.blue(`The Temperature in Los Angeles is currently ${res.body.currently.temperature} degrees Fahrenheit.  There is ${res.body.currently.precipProbability}% chance of rain.`));
+  console.log(chalk.blue(`The Temperature in Los Angeles is currently ${res.body.currently.temperature} degrees Fahrenheit.  There is ${res.body.currently.precipProbability}% chance of rain. It is predicted to be ${res.body.daily.data[0].summary.toLowerCase()}`));
 })
