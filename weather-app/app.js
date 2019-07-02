4702e0acab489a777b52409ab4c31d4389133e39
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-07-02 11:44:57 -0700

    separated geoCode into its own file

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,5 @@
 const request = require('request');
-const chalk = require('chalk');
+const chalk = require('chalk');const geoCode = require('./utils/geocode.js');
 
 // const URL ='https://api.darksky.net/forecast/479e3e61b28a471af898ef5eccc876f2/37.8267,-122.4233';
 
@@ -27,25 +27,9 @@ const chalk = require('chalk');
 //   }
 // });
 
-const geoCode = (address, callback) => {
-  const URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoicHVibGlrd2Vya2VyIiwiYSI6ImNqeGplMnd2bjB6bWozenFua3JxMXNvcnAifQ.jvtutPYzNjoYyzdK9VQnVA&?limit=1`
 
-  request({url: URL, json:true}, (err, res) => {
-    if (err) {
-      callback(`Unable to contact GEOCODING services`);
-    } else if (res.body.features.length === 0) {
-      callback(`Unable to find location. Try another search`);
-    } else {
-      console.log(res.body.features[0])
-      const place = res.body.features[0].text;
-      const lat = res.body.features[0].center[1];
-      const long = res.body.features[0].center[0]
-      console.log(chalk.yellow(`The latitude and longitude of ${place} is ${lat}, ${long}`));
-    }
-  })
-}
 
-geoCode('12Portland', (error, data) => { 
+geoCode('Portland', (error, data) => { 
   console.log('Error', error);
   console.log('Data', data);
 })
---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require('request');
+const geoCode = (address, callback) => {
+  const URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoicHVibGlrd2Vya2VyIiwiYSI6ImNqeGplMnd2bjB6bWozenFua3JxMXNvcnAifQ.jvtutPYzNjoYyzdK9VQnVA&?limit=1`
+
+  request({url: URL, json:true}, (err, res) => {
+    if (err) {
+      callback(`Unable to contact GEOCODING services`);
+    } else if (res.body.features.length === 0) {
+      callback(`Unable to find location. Try another search`);
+    } else {
+      console.log(res.body.features[0])
+      callback(undefined, {
+        place: res.body.features[0].place_name,
+        lat: res.body.features[0].center[1],
+        long: res.body.features[0].center[0],
+      })
+    }
+  })
+}
+
+module.exports = geoCode;
