13bd15e17e6408e50194f90544f97ad3f698a9c4
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-07-02 11:35:40 -0700

    weather app can display to console info for any city hardcoded in

--- weather-app/app.js
+++ weather-app/app.js
@@ -13,16 +13,39 @@ const chalk = require('chalk');
 //   }
 // })
 
-const geoURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/los%20angeles.json?access_token=pk.eyJ1IjoicHVibGlrd2Vya2VyIiwiYSI6ImNqeGplMnd2bjB6bWozenFua3JxMXNvcnAifQ.jvtutPYzNjoYyzdK9VQnVA&?limit=1';
+// const geoURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/los%20angeles.json?access_token=pk.eyJ1IjoicHVibGlrd2Vya2VyIiwiYSI6ImNqeGplMnd2bjB6bWozenFua3JxMXNvcnAifQ.jvtutPYzNjoYyzdK9VQnVA&?limit=1';
 
-request({url: geoURL, json: true}, (err, res) => {
-  if (err) {
-    console.log(chalk.red.inverse.bold(`Unable to contact GEOCODING services`))
-  } else if (!res.body.features){
-    console.log(chalk.red.inverse.bold(`Unable to find location`));
-  } else {
-    const lat = res.body.features[0].center[1];
-    const long = res.body.features[0].center[0]
-    console.log(chalk.yellow(`The latitude and longitude of Los Angeles is ${lat}, ${long}`));
-  }
-});
+// request({url: geoURL, json: true}, (err, res) => {
+//   if (err) {
+//     console.log(chalk.red.inverse.bold(`Unable to contact GEOCODING services`))
+//   } else if (!res.body.features){
+//     console.log(chalk.red.inverse.bold(`Unable to find location`));
+//   } else {
+//     const lat = res.body.features[0].center[1];
+//     const long = res.body.features[0].center[0]
+//     console.log(chalk.yellow(`The latitude and longitude of Los Angeles is ${lat}, ${long}`));
+//   }
+// });
+
+const geoCode = (address, callback) => {
+  const URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoicHVibGlrd2Vya2VyIiwiYSI6ImNqeGplMnd2bjB6bWozenFua3JxMXNvcnAifQ.jvtutPYzNjoYyzdK9VQnVA&?limit=1`
+
+  request({url: URL, json:true}, (err, res) => {
+    if (err) {
+      callback(`Unable to contact GEOCODING services`);
+    } else if (res.body.features.length === 0) {
+      callback(`Unable to find location. Try another search`);
+    } else {
+      console.log(res.body.features[0])
+      const place = res.body.features[0].text;
+      const lat = res.body.features[0].center[1];
+      const long = res.body.features[0].center[0]
+      console.log(chalk.yellow(`The latitude and longitude of ${place} is ${lat}, ${long}`));
+    }
+  })
+}
+
+geoCode('12Portland', (error, data) => { 
+  console.log('Error', error);
+  console.log('Data', data);
+})
