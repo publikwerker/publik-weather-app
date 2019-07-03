9edb63d7f7919166dab78939f18e705d2fd5f2d9
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-07-02 21:00:27 -0700

    handled exception if there is no city given

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,7 +4,9 @@ const chalk = require('chalk');
 const location = process.argv[2];
 
 geoCode(location, (error, data) => {
-  if (error){
+  if (!location){
+    return console.log(chalk.blue.bold.inverse(`Please provide city as command line argument`));
+  } else if (error){
     return console.log(chalk.red.inverse.bold('Error'), error);
   }
     forecast(data, (error, forecastData) => {
