274312715b0a194534bb605b0786157fceb6de55
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-07-03 10:31:39 -0700

    created four endpoints for server, including about, weather, help

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,8 +2,20 @@ const chalk = require('chalk');
 const express = require('express');
 const app = express();
 
-app.get('', (req, res)=>{
-  res.send('Hello express!')
+app.get('', (req, res) => {
+  res.send('Hello express!');
+});
+
+app.get('/help', (req, res) => {
+  res.send('Help page');
+});
+
+app.get('/about', (req, res) => {
+  res.send(`About page`);
+});
+
+app.get('/weather', (req, res) => {
+  res.send(`Weather page`);
 });
 
 //app.com
