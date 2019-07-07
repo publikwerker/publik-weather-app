6cdfddc954a2a4a69a66c7a2c5fc84d18037afb1
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-07-07 08:47:10 -0700

    fixed console messages

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -3,18 +3,18 @@ const chalk = require('chalk');
 
 const forecast = (data, callback) => {
   const {lat,long,place} = data;
-  const URL =`https://api.darksky.net/forecast/479e3e61b28a471af898ef5eccc876f2/${lat},${long}`;
+  const url =`https://api.darksky.net/forecast/479e3e61b28a471af898ef5eccc876f2/${lat},${long}`;
 
-  request({url: URL, json: true}, (err, res) => {
+  request({url, json: true}, (err, {body}) => {
     if (err){
       callback(chalk.red.inverse.bold(`Unable to access network connection`));
-    } else if (res.body.error) {
+    } else if (body.error) {
       callback(chalk.red.inverse.bold(`Unable to find location`));
     } else {
-      const temp = res.body.currently.temperature;
-      const precip = res.body.currently.precipProbability;
-      const text = res.body.daily.data[0].summary;
-      callback(undefined, chalk.yellow(`The temperature in ${place}, is ${temp} degrees Fahrenheit, with ${precip}% chance of rain. The forecast calls for ${text.toLowerCase()}`)
+      const {temperature} = body.currently;
+      const precip = body.currently.precipProbability;
+      const {summary} = body.daily.data[0];
+      callback(undefined, chalk.yellow(`The temperature in ${place}, is ${temperature} degrees Fahrenheit, with ${precip}% chance of rain. The forecast calls for ${summary.toLowerCase()}`)
       );
     };
   });
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -1,18 +1,18 @@
 const request = require('request');
 const chalk = require('chalk');
 const geoCode = (address, callback) => {
-  const URL = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoicHVibGlrd2Vya2VyIiwiYSI6ImNqeGplMnd2bjB6bWozenFua3JxMXNvcnAifQ.jvtutPYzNjoYyzdK9VQnVA&?limit=1`;
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoicHVibGlrd2Vya2VyIiwiYSI6ImNqeGplMnd2bjB6bWozenFua3JxMXNvcnAifQ.jvtutPYzNjoYyzdK9VQnVA&?limit=1`;
 
-  request({url: URL, json:true}, (err, res) => {
+  request({url, json:true}, (err, {body}) => {
     if (err) {
       return callback(chalk.red.inverse.bold(`Unable to contact GEOCODING services`));
-    } else if (res.body.features.length === 0) {
+    } else if (body.features.length === 0) {
       return callback(chalk.red.inverse.bold(`Unable to find location. Try another search`));
     } else {
       return callback(undefined, {
-        place: res.body.features[0].place_name,
-        lat: res.body.features[0].center[1],
-        long: res.body.features[0].center[0],
+        place: body.features[0].place_name,
+        lat: body.features[0].center[1],
+        long: body.features[0].center[0],
       })
     }
   })
