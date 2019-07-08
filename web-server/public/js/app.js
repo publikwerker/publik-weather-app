da8502dc394f4a5b99b401dc7edf1a556f108f5f
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-07-08 10:12:16 -0700

    added form to weather page. forecast for info submitted is displayed in the console.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,13 +1,20 @@
 console.log(`Client side javascript file is loaded!`);
-const address = 'Portland';
-fetch(`http://localhost:3000/weather?address=${address}`)
-.then((response)=>{
-  response.json().then((data)=>{
-    if(data.error){
-      console.log(data.error)
-    } else {
-      console.log(data.place);
-      console.log(data.summary);
-    };
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const address = search.value;
+  fetch(`http://localhost:3000/weather?address=${address}`)
+  .then((response)=>{
+    response.json().then((data)=>{
+      if(data.error){
+        console.log(data.error);
+      } else {
+        console.log(data.place);
+        console.log(data.summary);
+      };
+    });
   });
-});
+})
