f968989b2d49cf2a2d5a301819bb795ab8f92526
Jason Hoffman <jason.michael.hoffman@gmail.com>
2019-07-08 10:23:30 -0700

    errors, location, and forecast info now displays in DOM.

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,18 +2,23 @@ console.log(`Client side javascript file is loaded!`);
 
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
+const messageOne = document.querySelector('#messageOne');
+const messageTwo = document.querySelector('#messageTwo');
+
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const address = search.value;
+  messageOne.textContent = 'Loading...';
+  messageTwo.textContent = '';
   fetch(`http://localhost:3000/weather?address=${address}`)
   .then((response)=>{
     response.json().then((data)=>{
       if(data.error){
-        console.log(data.error);
+        messageOne.textContent = data.error;
       } else {
-        console.log(data.place);
-        console.log(data.summary);
+        messageOne.textContent = data.place;
+        messageTwo.textContent = data.summary;
       };
     });
   });
